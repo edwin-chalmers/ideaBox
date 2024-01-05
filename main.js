9b47bcd9fe0a98f74315d90caf36d1bac42bb3f1
Edwin Chalmers <edwinchalmers@gmail.com>
2024-01-04 18:02:18 -0700

    Add the generated idea objects to an array and data model

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ var saveButton = document.querySelector('#saveButton');
 var titleInput = document.querySelector('#title');
 var bodyInput = document.querySelector('#body')
 
+var ideaObjectArray = []
+
 saveButton.addEventListener("click", function () {
     var ideaObject = {
         title: titleInput.id,
         body: bodyInput.id,
         id: Date.now()
     }
-    console.log(ideaObject)
+    ideaObjectArray.push(ideaObject)
 })
