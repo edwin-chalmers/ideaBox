ad8f19f082843ee17111d124cb23db0c9b51490e
Edwin Chalmers <edwinchalmers@gmail.com>
2024-01-05 17:52:29 -0700

    complete iteration 2 with two ids
    Co-authored-by David Swatzell : <thatsstrikethree@gmail.com>
    Co-authored-by Lydia Sims : <sims.lydia88@gmail.com>

--- main.js
+++ main.js
@@ -33,10 +33,16 @@ function saveState() {
 
 function insertCard(title, body, id) {
     ideasDiv.insertAdjacentHTML('afterbegin', 
-        `<div class="idea-containers" id="${id}">
-        <h3>${title}</h3>
-        <p> ${body}</p>
-        </div>`)
+    `<div class="idea-containers" id="${id}">
+        <div class="purple-box">
+            <img id="${id}" class="delete-icon" src="./assets/delete.svg" alt="delete-icon">
+            <img class="star-icon" src="./assets/star.svg" alt="star-icon">
+        </div> 
+        <div class="title-body">
+            <h3>${title}</h3>
+            <p> ${body}</p>
+        </div>
+    </div>`)
 }
 
 function clearForm() {
@@ -47,7 +53,7 @@ function clearForm() {
 
 ///// ?????
 ideasDiv.addEventListener("click", function(event) {
-    var targetElement = event.target.closest('.idea-containers')
+    var targetElement = event.target.closest('.delete-icon')
     
     if (targetElement) {
         var cardID = targetElement.getAttribute('id')
